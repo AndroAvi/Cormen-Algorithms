7c1b178d841783175219fe89f3a14176de89d003
Avijit Pandey <avijeetpandey87@gmail.com>
2020-12-12 13:14:16 +0530

    Added Insert procedure for youngs tableaue

--- youngs-tablaue.cpp
+++ youngs-tablaue.cpp
@@ -4,12 +4,37 @@
 #include <utility>
 #include <climits>
 
-void fillTab(std::vector<std::vector<int>> &tab){
+void addPaddings(std::vector<std::vector<int>> &tab){
 	int row = int(tab.size());
 	int col = int(tab[0].size());
-	tab.resize( row + 1);
-	for(int i{0}; i <= row; i++)
-		tab[i].resize(col + 1);
+	std::vector<std::vector<int>> cTab = tab;
+	tab.resize(row + 2);
+	for(auto &row : tab)
+		row.resize(col + 2);
+	for(int j{0}; j < col + 2; j++)
+		tab[0][j] = INT_MIN;
+	for(int i{0};i < row + 2; i++)
+		tab[i][0] = INT_MIN;
+	for(int i{1}; i <= row; i++){
+		for(int j{1}; j <= col; j++)
+			tab[i][j] = cTab[i - 1][j - 1];
+	}
+}
+
+void removePad(std::vector<std::vector<int>> &tab){
+	int row = int(tab.size() - 2);
+	int col = int(tab[0].size()) - 2;
+	std::vector<std::vector<int>> cTab = tab;
+	tab.resize(row);
+	for(auto &row : tab)
+		row.resize(col);
+	for(int i{1}; i <= row; i++)
+		for(int j{1}; j <= col; j++)
+			tab[i - 1][j - 1] = cTab[i][j];
+}
+
+void fillTab(std::vector<std::vector<int>> &tab){
+	addPaddings(tab);
 	for(auto &row : tab){
 		for(auto &el : row)
 			if(el == 0)
@@ -39,6 +64,8 @@ void printArr(std::vector<std::vector<int>> &tab){
 		for(auto &el : row){
 			if(el == INT_MAX)
 				std::cout << char(236) << "    ";
+			else if(el == INT_MIN)
+				std::cout << '-' << char(236) << "    ";
 			else
 				std::cout << el << "    ";
 		}
@@ -46,22 +73,56 @@ void printArr(std::vector<std::vector<int>> &tab){
 	}
 }
 
+void insert(std::vector<std::vector<int>> &tab, int val){
+	int row = int(tab.size()) - 2;
+	int col = int(tab[0].size()) - 2;
+	tab[row][col] = val;
+	while(tab[row - 1][col] > tab[row][col] or tab[row][col - 1] > tab[row][col]){
+		if(tab[row-1][col] > tab[row][col - 1]){
+			std::swap(tab[row][col], tab[row - 1][col]);
+			row--;
+		}else{
+			std::swap(tab[row][col], tab[row][col - 1]);
+			col--;
+		}
+	}
+}
+
 
 int main(){
+	/* Driver Code
 	int row, col;
 	std::cout << "Enter the number of rows and columns: ";
 	std::cin >> row >> col;
 	std::vector<std::vector<int>> tab(row, std::vector<int>(col));
+	std::cout << "Enter the elements of the tableau:\n";
 	for(auto &row : tab)
 		for(auto &el : row)
 			std::cin >> el;
+	std::cout << "Input Tableau:\n";
+	printArr(tab);
+	
+	//Testing Code for EXTRACT-MIN() Procedure in O(row + col) time.
+	std::cout << "Enter row and column indeces of sub tableau to extract min from: ";
+	int i, j;
+	std::cin >> i >> j;
 	fillTab(tab);
-	std::cout << "Input Matrix:\n";
-	printArr(tab);
-	std::cout << "After extracting " << extractMin(tab, 0, 0) << ":\n";
+	std::cout << "After extracting " << extractMin(tab, i+1, j+1) << ":\n";
+	removePad(tab);
 	printArr(tab);
 
 
+	
+	//Testing Code for INSERT() Procedure in O(row + col) time.
+	std::cout << "Enter element to insert: ";
+	int val;
+	std::cin >> val;
+	fillTab(tab);
+	insert(tab, val);
+	removePad(tab);
+	std::cout << "After inserting " << val << ":\n";
+	printArr(tab);
+	*/
 
 	std::cout << std::endl;
 	return 0;
